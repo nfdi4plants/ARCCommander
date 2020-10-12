079e1c799cfbe8c4d007576de94d5de30bac632e
Timo Mühlhaus <muehlhaus@bio.uni-kl.de>
2020-10-12 18:49:26 +0200

    ArcCommander command line tool

---
+++ src/ArcCommander/Arc.fs
@@ -0,0 +1,16 @@
+namespace ArcCommander
+
+open System
+
+module Arc =
+
+    // Creates Arc specific folder structure 
+    let init (name:string) =
+        NotImplementedException() |> raise
+
+    // Returns true if called anywhere in an arc 
+    let isArc () =
+        NotImplementedException() |> raise
+
+    let addAssay (name:string) = 
+        NotImplementedException() |> raise
