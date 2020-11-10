6ec80056cebb3199e9a465aba0d24faa46bdd0f4
Kevin Schneider <kevin-schneider@mutagene.de>
2020-11-10 17:46:57 +0100

    Further unify APIs for CLI handling - add inline documentation to all functions/stubs - replace `create` command verb  with `init` for studies and assays - fix wrong CLI argument mapping for some Verbs

--- src/ArcCommander/CLIArguments/Assay.fs
+++ src/ArcCommander/CLIArguments/Assay.fs
@@ -2,31 +2,16 @@
 open Argu 
 open ISA
 
-type AssayCreateArgs =
-    | [<Mandatory>][<AltCommandLine("-s")>][<Unique>] StudyIdentifier of study_identifier:string
+/// CLI arguments for empty assay initialization
+type AssayInitArgs =
     | [<Mandatory>][<AltCommandLine("-a")>][<Unique>] AssayIdentifier of assay_identifier:string
 
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | StudyIdentifier   _ -> "identifier of the study in which the assay is registered"
-            | AssayIdentifier   _ -> "identifier of the assay of interest"
+            | AssayIdentifier   _ -> "identifier of the assay, will be used as name of the root folder of the new assay folder structure"
 
-type AssayEditArgs = AssayCreateArgs
-type AssayRemoveArgs = AssayCreateArgs
-
-type AssayMoveArguments =
-    | [<Mandatory>][<AltCommandLine("-s")>][<Unique>] StudyIdentifier of study_identifier:string
-    | [<Mandatory>][<AltCommandLine("-a")>][<Unique>] AssayIdentifier of assay_identifier:string
-    | [<Mandatory>][<AltCommandLine("-t")>][<Unique>] TargetStudyIdentifier of target_study_identifier:string
-
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | StudyIdentifier   _ -> "Name of the study in which the assay is situated"
-            | AssayIdentifier   _ -> "Name of the assay of interest"
-            | TargetStudyIdentifier _-> "Target study"
-
+/// CLI arguments for updating existing assay metadata
 type AssayUpdateArgs =  
     | [<Mandatory>][<AltCommandLine("-s")>][<Unique>] StudyIdentifier of string
     | [<Mandatory>][<AltCommandLine("-a")>][<Unique>] AssayIdentifier of string
@@ -51,29 +36,65 @@ type AssayUpdateArgs =
             | TechnologyTypeTermSourceREF _         -> "Technology Type Term Source REF of the assay"
             | TechnologyPlatform _                  -> "Technology Platform of the assay"
 
+/// CLI arguments for interactively editing existing assay metadata 
+// Same arguments as `update` because edit is basically an interactive update, where 
+// the arguments set in the command line will be already set when opening the editor
+type AssayEditArgs = AssayUpdateArgs
+
+/// CLI arguments for registering existing assay metadata 
+// Same arguments as `update` because all metadata fields that can be updated can also be set while registering
 type AssayRegisterArgs = AssayUpdateArgs
+
+/// CLI arguments for initializing and subsequently registering assay metadata 
+// Same arguments as `update` because all metadata fields that can be updated can also be set while registering a new assay
 type AssayAddArgs = AssayUpdateArgs
 
+/// CLI arguments for assay removal
+type AssayRemoveArgs =
+    | [<Mandatory>][<AltCommandLine("-s")>][<Unique>] StudyIdentifier of study_identifier:string
+    | [<Mandatory>][<AltCommandLine("-a")>][<Unique>] AssayIdentifier of assay_identifier:string
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | StudyIdentifier   _ -> "identifier of the study in which the assay is registered"
+            | AssayIdentifier   _ -> "identifier of the assay of interest"
+
+/// CLI arguments for assay move between studies
+type AssayMoveArguments =
+    | [<Mandatory>][<AltCommandLine("-s")>][<Unique>] StudyIdentifier of study_identifier:string
+    | [<Mandatory>][<AltCommandLine("-a")>][<Unique>] AssayIdentifier of assay_identifier:string
+    | [<Mandatory>][<AltCommandLine("-t")>][<Unique>] TargetStudyIdentifier of target_study_identifier:string
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | StudyIdentifier   _ -> "Name of the study in which the assay is situated"
+            | AssayIdentifier   _ -> "Name of the assay of interest"
+            | TargetStudyIdentifier _-> "Target study"
+
+
+/// Assay object subcommand verbs
 type Assay = 
-    | [<CliPrefix(CliPrefix.None)>] Create   of create_args:  ParseResults<AssayCreateArgs>
+    | [<CliPrefix(CliPrefix.None)>] Init     of create_args:  ParseResults<AssayInitArgs>
     | [<CliPrefix(CliPrefix.None)>] Update   of update_args:  ParseResults<AssayUpdateArgs>
     | [<CliPrefix(CliPrefix.None)>] Edit     of edit_args:    ParseResults<AssayEditArgs>
     | [<CliPrefix(CliPrefix.None)>] Register of register_args:ParseResults<AssayRegisterArgs>
     | [<CliPrefix(CliPrefix.None)>] Add      of add_args:     ParseResults<AssayAddArgs>
-    | [<CliPrefix(CliPrefix.None)>] Move     of move_args:    ParseResults<AssayMoveArguments>
     | [<CliPrefix(CliPrefix.None)>] Remove   of remove_args:  ParseResults<AssayRemoveArgs>
+    | [<CliPrefix(CliPrefix.None)>] Move     of move_args:    ParseResults<AssayMoveArguments>
     | [<CliPrefix(CliPrefix.None)>] [<SubCommand()>] List
 
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | Create            _ -> "Create a new assay with the given parameters"
-            | Update            _ -> "Update an assaywith the given parameters"
-            | Edit              _ -> "Open an editor window to directly edit an assay"
-            | Register          _ -> "Register an existing assay in the given study"
-            | Add               _ -> "Create a new assay and register it to the given study"
+            | Init              _ -> "Initialize a new empty assay and associated folder structure in the arc."
+            | Update            _ -> "Update an existing assay in the arc with the given assay metadata"
+            | Edit              _ -> "Open and edit an existing assay in the arc with a text editor. Arguments passed for this command will be pre-set in the editor."
+            | Register          _ -> "Register an existing assay in the arc with the given assay metadata."
+            | Add               _ -> "Create a new assay file and associated folder structure in the arc and subsequently register it with the given assay metadata"
+            | Remove            _ -> "Remove an assay from the given studys' assay register"
             | Move              _ -> "Move an assay from one study to another"
-            | Remove            _ -> "Remove an assay from the arc"
-            | List              _ -> "List all assays registered in the investigation file"
+            | List              _ -> "List all assays registered in the arc"
 
 
--- src/ArcCommander/CLIArguments/Study.fs
+++ src/ArcCommander/CLIArguments/Study.fs
@@ -3,7 +3,17 @@
 open Argu 
 open ISA
 
-type StudyCreateArgs =
+/// CLI arguments for empty study initialization
+type StudyInitArgs =
+    | [<Mandatory>][<Unique>] Identifier of study_identifier:string
+
+    interface IArgParserTemplate with
+        member this.Usage =
+            match this with
+            | Identifier _->    "Identifier of the study, will be used as the file name of the study file"
+
+/// CLI arguments for updating existing study metadata
+type StudyUpdateArgs =
     | [<Mandatory>][<Unique>] Identifier of study_identifier:string
     | [<Unique>] Title of title:string
     | [<Unique>] Description of description:string
@@ -19,37 +29,41 @@ type StudyCreateArgs =
             | SubmissionDate _->   "Submission Date of the study"
             | PublicReleaseDate _->   "Public Release Date of the study"
 
-type StudyUpdateArgs = StudyCreateArgs
-type StudyRegisterArgs = StudyCreateArgs
-type StudyAddArgs = StudyCreateArgs
+/// CLI arguments for interactively editing existing study metadata 
+// Same arguments as `update` because edit is basically an interactive update, where 
+// the arguments set in the command line will be already set when opening the editor
+type StudyEditArgs = StudyUpdateArgs
 
-type StudyEditArgs =
-    | [<Mandatory>][<Unique>] Identifier of study_identifier:string
+/// CLI arguments for registering existing study metadata 
+// Same arguments as `update` because all metadata fields that can be updated can also be set while registering
+type StudyRegisterArgs = StudyUpdateArgs
 
-    interface IArgParserTemplate with
-        member this.Usage =
-            match this with
-            | Identifier _->    "Identifier of the study, will be used as the file name of the study file"
+/// CLI arguments for initializing and subsequently registering study metadata 
+// Same arguments as `update` because all metadata fields that can be updated can also be set while registering a new assay
+type StudyAddArgs = StudyUpdateArgs
 
-type StudyRemoveArgs = StudyEditArgs
+/// CLI arguments for study removal
+// same as `init` because both commands only need to be passed a study identifier
+type StudyRemoveArgs = StudyInitArgs
 
+/// Study object subcommand verbs
 type Study =
 
-    | [<CliPrefix(CliPrefix.None)>] Create of create_args:ParseResults<StudyCreateArgs>
-    | [<CliPrefix(CliPrefix.None)>] Update of update_args:ParseResults<StudyUpdateArgs>
-    | [<CliPrefix(CliPrefix.None)>] Edit of edit_args:ParseResults<StudyEditArgs>
+    | [<CliPrefix(CliPrefix.None)>] Init     of create_args:ParseResults<StudyInitArgs>
+    | [<CliPrefix(CliPrefix.None)>] Update   of update_args:ParseResults<StudyUpdateArgs>
+    | [<CliPrefix(CliPrefix.None)>] Edit     of edit_args:ParseResults<StudyEditArgs>
     | [<CliPrefix(CliPrefix.None)>] Register of register_args:ParseResults<StudyRegisterArgs>
-    | [<CliPrefix(CliPrefix.None)>] Add of add_args:ParseResults<StudyAddArgs>
-    | [<CliPrefix(CliPrefix.None)>] Remove of remove_args:ParseResults<StudyRemoveArgs>
+    | [<CliPrefix(CliPrefix.None)>] Add      of add_args:ParseResults<StudyAddArgs>
+    | [<CliPrefix(CliPrefix.None)>] Remove   of remove_args:ParseResults<StudyRemoveArgs>
     | [<CliPrefix(CliPrefix.None)>] [<SubCommand()>] List 
 
     interface IArgParserTemplate with
         member this.Usage =
             match this with
-            | Create    _ -> "Create a new study with the given parameters"
-            | Update    _ -> "Update a study with the given parameters"
-            | Edit      _ -> "Open an editor window to directly edit a study"
-            | Register  _ -> "Register an existing study in the given investigation"
-            | Add       _ -> "Create a new study and add it to the given investigation"
+            | Init      _ -> "Initialize a new empty study file in the arc"
+            | Update    _ -> "Update an existing study in the arc with the given study metadata"
+            | Edit      _ -> "Open and edit an existing study in the arc with a text editor. Arguments passed for this command will be pre-set in the editor."
+            | Register  _ -> "Register an existing study in the arc with the given assay metadata."
+            | Add       _ -> "Create a new study file in the arc and subsequently register it with the given study metadata"
             | Remove    _ -> "Remove a study from the arc"
-            | List      _ -> "List all studies registered in the given investigation"
+            | List      _ -> "List all studies registered in the arc"
