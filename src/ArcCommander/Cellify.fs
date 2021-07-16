989aee84fe88e31cd7a9c1b4bc1661b61c27aac6
omaus <omaus@bio.uni-kl.de>
2021-07-16 14:57:36 +0200

    Add cellify source file

---
+++ src/ArcCommander/Cellify.fs
@@ -0,0 +1,45 @@
+﻿module Cellify
+
+open FSharpSpreadsheetML
+
+/// Reads an .xlsx file and prints the coordinates and values of the cells of its sheets.
+let parse infile =
+
+    let doc = Spreadsheet.fromFile infile false
+    
+    let sst = Spreadsheet.tryGetSharedStringTable doc
+
+    let sheetNames = 
+        Spreadsheet.getWorkbookPart doc
+        |> Workbook.get
+        |> Sheet.Sheets.get
+        |> Sheet.Sheets.getSheets
+        |> Seq.map Sheet.getName
+        |> Array.ofSeq
+
+    let res =
+        sheetNames
+        |> Seq.iter (fun sheetName ->                    
+            match Spreadsheet.tryGetWorksheetPartBySheetName sheetName doc with
+            | Some wsp ->
+                let sheet = Worksheet.getSheetData wsp.Worksheet
+                printfn "[%s]" sheetName
+                printfn "================================="
+                SheetData.getRows sheet
+                |> Seq.iter (fun row -> 
+                    row 
+                    |> Row.toCellSeq
+                    |> Seq.iter (fun c -> printfn "%s : %s" c.CellReference.Value (Cell.getValue sst c))
+                    )
+            
+                printfn "================================="
+            | None -> ()    
+        )
+
+    Spreadsheet.close doc
+
+    res
+
+let main () =
+    let argsNo = System.Environment.GetCommandLineArgs()
+    parse (Array.last argsNo)
