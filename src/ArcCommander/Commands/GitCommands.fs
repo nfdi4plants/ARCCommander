4f5536d1b86e95b13fdab0d6d2ce7a272b9db64a
omaus <omaus@bio.uni-kl.de>
2021-09-05 23:53:38 +0200

    Update git commands

--- src/ArcCommander/Commands/GitCommands.fs
+++ src/ArcCommander/Commands/GitCommands.fs
@@ -8,9 +8,11 @@ type GitCommand =
 
     | [<CliPrefix(CliPrefix.None)>] Init    of init_args:   ParseResults<GitInitArgs>
     | [<CliPrefix(CliPrefix.None)>] Sync    of sync_args:   ParseResults<GitSyncArgs>
+    | [<CliPrefix(CliPrefix.None)>] [<SubCommand()>] Diff
 
     interface IArgParserTemplate with
         member this.Usage =
             match this with
             | Init      _ -> "Set up git locally for this arc"
             | Sync      _ -> "Commits changes made in the arc. If a remote is set or is given, also pulls from there and pushes all previously made commits."
+            | Diff      _ -> "Shows the differences between all local and remote files of the current branch."
