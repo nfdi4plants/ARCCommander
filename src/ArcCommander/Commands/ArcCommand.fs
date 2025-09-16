0fa4fceb1f37f3522e0ead8f627d40485effaeb7
Oliver Maus <oliver.maus@eclipso.de>
2025-09-16 18:58:04 +0200

    Add test message function

--- src/ArcCommander/Commands/ArcCommand.fs
+++ src/ArcCommander/Commands/ArcCommand.fs
@@ -17,6 +17,7 @@ type ArcCommand =
     | [<CliPrefix(CliPrefix.None)>]                             Server          of server_args  : ParseResults<ArcServerArgs>
     | [<CliPrefix(CliPrefix.None)>][<SubCommand()>]             Update
     | [<AltCommandLine("--version")>][<CliPrefix(CliPrefix.None)>][<SubCommand()>] Version
+    | [<AltCommandLine("--testmessage")>][<CliPrefix(CliPrefix.None)>][<SubCommand()>] TestMessage
     ///Subcommands
     | [<AltCommandLine("i")>][<CliPrefix(CliPrefix.None)>]      Investigation   of verb_and_args : ParseResults<InvestigationCommand>
     | [<AltCommandLine("s")>][<CliPrefix(CliPrefix.None)>]      Study           of verb_and_args : ParseResults<StudyCommand>
@@ -41,4 +42,5 @@ type ArcCommand =
             | Assay         _   -> "Assay functions"
             | Configuration _   -> "Configuration editing"
             | Version           -> "Get the ArcCommander's current version"
+            | TestMessage       -> "Display a test message to check for correct coloring"
             | Server        _   -> "Start the ArcCommander as a local server"
